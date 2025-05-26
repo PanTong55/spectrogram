c69c989952500c28b1f1f9770379cc3aa85a2a6f
PanTong55 <pantong0505@gmail.com>
2025-05-26 13:53:46 +0800

    Update frequencyRangeControl.js

--- modules/frequencyRangeControl.js
+++ modules/frequencyRangeControl.js
@@ -21,17 +21,14 @@ export function initFrequencyRangeControl({
 
   function updateFrequencyRange(freqMin, freqMax) {
     const colorMap = getCurrentColorMap();
-    currentFreqMin = freqMin;
-    currentFreqMax = freqMax;
-
+  
     replacePlugin(colorMap, spectrogramHeight, freqMin, freqMax);
-    renderAxes();
+    renderAxes(freqMin, freqMax); // ✅ 用當前這次的設定去 render
+  
     setTimeout(() => {
       const ws = getWavesurfer();
-      if (ws) {
-        zoomControl.applyZoom();
-      }
-      onRangeUpdated(freqMin, freqMax);
+      if (ws) zoomControl.applyZoom();
+      onRangeUpdated(freqMin, freqMax); // 這時候才更新 state
     }, 100);
   }
 
