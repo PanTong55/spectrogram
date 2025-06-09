01d440ecd81c0d9bbdfe105c1395065ee70c85d5
PanTong55 <pantong0505@gmail.com>
2025-06-10 00:17:07 +0800

    Update fileState.js

--- modules/fileState.js
+++ modules/fileState.js
@@ -2,10 +2,12 @@
 
 let fileList = [];
 let currentIndex = -1;
+let fileIcons = {}; // { index: { trash: bool, star: bool, question: bool } }
 
 export function setFileList(list, index = 0) {
   fileList = list;
   currentIndex = index;
+  fileIcons = {};  
 }
 
 export function getFileList() {
@@ -27,4 +29,17 @@ export function getCurrentFile() {
     return fileList[currentIndex];
   }
   return null;
+}
+
+export function toggleFileIcon(index, type) {
+  if (!fileIcons[index]) {
+    fileIcons[index] = { trash: false, star: false, question: false };
+  }
+  if (type in fileIcons[index]) {
+    fileIcons[index][type] = !fileIcons[index][type];
+  }
+}
+
+export function getFileIconState(index) {
+  return fileIcons[index] || { trash: false, star: false, question: false };
 }
