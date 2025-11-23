da0f94d9a5a26d9c75b5c5e18b66445cd297a616
pantongcut <pantong.cut@gmail.com>
2025-11-23 08:48:40 +0000

    try fix knee time

--- modules/batCallDetector.js
+++ modules/batCallDetector.js
@@ -790,18 +790,35 @@ export class BatCallDetector {
     }
     
     // STEP 6.8: Set knee frequency and knee time from detected knee point
+    // 
+    // IMPORTANT: kneeIdx is an index into frameFrequencies array, which has
+    // the same length as spectrogram. timeFrames[0] is the start of this spectrogram segment.
+    // To get knee time relative to the call START, we need to account for any 
+    // refinement done in STEP 1.5 (where startTime_s may have been updated).
+    //
+    // The correct formula is:
+    // kneeTime_ms = (timeFrames[kneeIdx] - timeFrames[0]) * 1000
+    // This gives time relative to spectrogram start, which equals call duration start point.
     if (kneeIdx >= 0 && kneeIdx < frameFrequencies.length) {
       // Use original (non-smoothed) frequency at knee point for accuracy
       call.kneeFreq_kHz = frameFrequencies[kneeIdx] / 1000;
       
-      // Knee time relative to call start
-      if (kneeIdx < timeFrames.length) {
-        call.kneeTime_ms = (timeFrames[kneeIdx] - call.startTime_s) * 1000;
+      // Knee time relative to spectrogram start (which is refined call start from STEP 1.5)
+      if (kneeIdx >= 0 && kneeIdx < timeFrames.length) {
+        // timeFrames[0] is always the start of the spectrogram segment
+        // which after STEP 1.5 should match call.startTime_s
+        call.kneeTime_ms = (timeFrames[kneeIdx] - timeFrames[0]) * 1000;
+      } else {
+        call.kneeTime_ms = 0;
       }
     } else {
       // Ultimate fallback: use peak frequency
       call.kneeFreq_kHz = peakFreq_Hz / 1000;
-      call.kneeTime_ms = (timeFrames[peakFrameIdx] - call.startTime_s) * 1000;
+      if (peakFrameIdx >= 0 && peakFrameIdx < timeFrames.length) {
+        call.kneeTime_ms = (timeFrames[peakFrameIdx] - timeFrames[0]) * 1000;
+      } else {
+        call.kneeTime_ms = 0;
+      }
     }
   }
   
