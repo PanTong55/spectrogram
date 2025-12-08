9efbe985a7426d3f0226e4fde7cdb4fe48003c6d
PanTong55 <pantong0505@gmail.com>
2025-12-08 05:57:07 +0000

    fix rainbow error

--- modules/spectrogram.esm.js
+++ modules/spectrogram.esm.js
@@ -279,10 +279,10 @@ function generateColorMapRGBA(mapName) {
             { pos: 0.0, r: 255, g: 255, b: 255 },
             { pos: 0.25, r: 255, g: 255, b: 255 }, 
             { pos: 0.35, r: 255, g: 127, b: 128 },   // 噪聲起點 (Light Pinkish Red)
-            { pos: 0.45, r: 255, g: 255, b: 0 },  // 蝙蝠叫聲的外圍輪廓，訊號邊緣/過渡區 (Yellow)
+            { pos: 0.45, r: 255, g: 255, b: 0 },  // 外圍輪廓，訊號邊緣/過渡區 (Yellow)
             { pos: 0.65, r: 0, g: 255, b: 0 },   // 訊號主體 (Vibrant Green)
             { pos: 0.7, r: 0, g: 255, b: 255 },    // 強訊號區 (Cyan)
-            { pos: 0.9, r: 0, g: 0, b: 175 }   // (Blue)
+            { pos: 0.9, r: 0, g: 0, b: 175 },   // (Blue)
             { pos: 1.0, r: 0, g: 0, b: 39 }   // (Dark blue)
         ],
     };
