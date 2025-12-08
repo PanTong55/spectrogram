d47a10aa2d05fc8d213dcc000a16a5d61c60320c
PanTong55 <pantong0505@gmail.com>
2025-12-08 00:56:18 +0000

    adjust color map 2

--- modules/spectrogram.esm.js
+++ modules/spectrogram.esm.js
@@ -224,16 +224,16 @@ function generateColorMapRGBA(mapName) {
         // Existing Scientific Maps
         inferno: [
             { pos: 0.0, r: 0, g: 0, b: 4 },
-            { pos: 0.4, r: 87, g: 16, b: 109 },
-            { pos: 0.6, r: 188, g: 48, b: 60 },
+            { pos: 0.5, r: 87, g: 16, b: 109 },
+            { pos: 0.75, r: 188, g: 48, b: 60 },
             { pos: 0.85, r: 253, g: 128, b: 25 },
             { pos: 1.0, r: 252, g: 255, b: 164 }
         ],
         viridis: [
             { pos: 0.0, r: 68, g: 1, b: 84 },
-            { pos: 0.25, r: 59, g: 82, b: 139 },
-            { pos: 0.5, r: 33, g: 145, b: 140 },
-            { pos: 0.75, r: 253, g: 231, b: 37 },
+            { pos: 0.5, r: 59, g: 82, b: 139 },
+            { pos: 0.75, r: 33, g: 145, b: 140 },
+            { pos: 0.85, r: 253, g: 231, b: 37 },
             { pos: 1.0, r: 255, g: 255, b: 0 }
         ],
         magma: [
