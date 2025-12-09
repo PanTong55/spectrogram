585158b4951a109c316734873371e22f98106a74
PanTong55 <pantong0505@gmail.com>
2025-12-09 06:02:35 +0000

    remove middle green

--- modules/spectrogram.esm.js
+++ modules/spectrogram.esm.js
@@ -272,8 +272,7 @@ function generateColorMapRGBA(mapName, gain = 1.0) {
             { pos: 0.0, r: 0, g: 0, b: 0 }, 
             { pos: 0.01, r: 0, g: 0, b: 3 },
             { pos: 0.15, r: 0, g: 0, b: 0 }, 
-            { pos: 0.2, r: 0, g: 60, b: 90 },
-            { pos: 0.35, r: 0, g: 133, b: 44 },              
+            { pos: 0.2, r: 0, g: 60, b: 90 },           
             { pos: 0.5, r: 0, g: 180, b: 60 }, 
             { pos: 0.85, r: 255, g: 230, b: 0 }, 
             { pos: 1.0, r: 255, g: 40, b: 0 } 
