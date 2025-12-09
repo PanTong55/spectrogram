a0e5a7601c7eab22f8bd904d2f82ee1e1a60ba49
PanTong55 <pantong0505@gmail.com>
2025-12-09 05:24:13 +0000

    green

--- modules/spectrogram.esm.js
+++ modules/spectrogram.esm.js
@@ -270,8 +270,9 @@ function generateColorMapRGBA(mapName, gain = 1.0) {
         ],
         kaleidoscope: [
             { pos: 0.0, r: 0, g: 0, b: 0 }, 
-            { pos: 0.20, r: 0, g: 0, b: 0 }, 
-            { pos: 0.25, r: 0, g: 80, b: 120 },             
+            { pos: 0.10, r: 0, g: 0, b: 0 }, 
+            { pos: 0.15, r: 0, g: 80, b: 120 },
+            { pos: 0.30, r: 0, g: 133, b: 44 },              
             { pos: 0.45, r: 0, g: 180, b: 60 }, 
             { pos: 0.80, r: 255, g: 230, b: 0 }, 
             { pos: 1.0, r: 255, g: 40, b: 0 } 
